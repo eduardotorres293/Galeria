6dc70bb6a41b42795905ce48c2064c6fbb69aa49
pc <al23760375@ite.edu.mx>
2025-02-14 13:01:48 -0800

    Agregar archivos de proyecto.

---
+++ Practica2/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica2
+{
+    public partial class Form1 : Form
+    {
+        private string[] imagenes;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CargarImagenes();
+            MostrarImagen();
+        }
+
+        private void CargarImagenes()
+        {
+            string carpeta = Path.Combine(Application.StartupPath, @"..\..\Imagenes");
+            if (Directory.Exists(carpeta))
+            {
+                imagenes = Directory.GetFiles(carpeta, "*.jpg").Concat(Directory.GetFiles(carpeta, "*.png")).ToArray();
+            }
+            else
+            {
+                MessageBox.Show("La carpeta no existe o no está asignada");
+                imagenes = new string[0];
+            }
+        }
+
+        private void MostrarImagen()
+        {
+            flowLayoutPanel2.Controls.Clear();
+
+            if (imagenes.Length > 0)
+            {
+                foreach (var imagen in imagenes)
+                {
+                    PictureBox pictureBox = new PictureBox
+                    {
+                        ImageLocation = imagen,
+                        SizeMode = PictureBoxSizeMode.StretchImage,
+                        Width = 200,
+                        Height = 200,
+                        Margin = new Padding(10)
+                    };
+                    pictureBox.Click += (sender, e) => abrirForm(imagen);
+                    flowLayoutPanel2.Controls.Add(pictureBox);
+                }
+            }
+        }
+        private void abrirForm(string imagen)
+        {
+            string nombreImagen = Path.GetFileNameWithoutExtension(imagen);
+            imagenGrande imagenFormulario = new imagenGrande(imagen, nombreImagen);
+            imagenFormulario.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string openFolder = Path.Combine(Application.StartupPath, @"..\..\Imagenes");
+            System.Diagnostics.Process.Start(openFolder);
+        }
+    }
+}
---
+++ Practica2/imagenGrande.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica2
+{
+    public partial class imagenGrande : Form
+    {
+        public imagenGrande(string imagenPath, string nombreImagen)
+        {
+            InitializeComponent();
+            pictureBox1.ImageLocation = imagenPath;
+            this.Text = nombreImagen;
+        }
+    }
+}
